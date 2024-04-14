4c6c372112df31073bf56bb46607f7dcb30afc91
aymatur <aym.matsuura@gmail.com>
2024-04-14 22:55:14 +0900

    ch04 2.1.2

---
+++ ch04/06_callback_deferred_execution/test.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const cache = {};
+function consistentReadAsync(filename, callback) {
+    if (cache[filename]) {
+        process.nextTick(() => { callback(cache[filename])});
+    } else {
+        fs.readFile(filename, 'utf8', (err, data) => {
+            cache[filename] = data;
+            callback(data);
+        });
+    }
+}
+
+function createFileReader(filename) {
+    const listeners = [];
+    consistentReadAsync(filename, value => {
+        listeners.forEach(listener => listener(value));
+    });
+    return {
+        onDataReady: listener => listeners.push(listener)
+    };
+}
+
+const reader1 = createFileReader('data.txt');
+console.log(reader1);
+reader1.onDataReady(data => {
+    console.log('First call data: ' + data);
+    const reader2 = createFileReader('data.txt');
+    reader2.onDataReady(data => {
+        console.log('Second call data: ' + data);
+    });
+});
