be398ffb3ad6146678a35e4159ff87be3758c9ed
aymatur <aym.matsuura@gmail.com>
2024-04-22 23:24:17 +0900

    ch04 2.2.2.2

---
+++ ch04/09_module_revealing_module/test.js
@@ -0,0 +1,12 @@
+const mod = (() => {
+    const privateFoo = () => {};
+    const privateBar = [];
+
+    const exported = {
+        publicFoo: () => {},
+        publicBar: () => {}
+    };
+    return exported;
+})();
+
+console.log(mod);
