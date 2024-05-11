5875d49f9c14beccea193f4ad2fd807d02ec1b68
aymatur <aym.matsuura@gmail.com>
2024-05-12 02:08:15 +0900

    4.1.4.1

---
+++ ch04/02_promises_sequential_execution/index.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const path = require('path');
+const utilities = require('./utilities');
+
+const request = utilities.promisify(require('request'));
+const fs = require('fs');
+const mkdirp = utilities.promisify(require('mkdirp'));
+const readFile = utilities.promisify(fs.readFile);
+const writeFile = utilities.promisify(fs.writeFile);
+
+function spiderLinks(currentUrl, body, nesting) {
+    let promise = Promise.resolve();
+    if (nesting === 0) {
+        return promise;
+    }
+    const links = utilities.getPageLinks(currentUrl, body);
+    links.forEach(link => {
+        promise = promise.then(() => spider(link, nesting - 1));
+    });
+    return promise;
+}
+
+function download(url, filename) {
+    console.log(`Downloading url ${url}`);
+    let body;
+    return request(url)
+        .then(response => {
+            body = reqponse.body;
+            return mkdirp(path.dirname(filename));
+        })
+        .then(() => writeFile(filename, body))
+        .then(() => {
+            console.log(`Downloaded and saved: ${url}`);
+            return body;
+        })
+    ;
+}
+
+function spider(url, nesting) {
+    let filename = utilities.urlToFilename(url);
+    return readFile(filename, 'utf8')
+        .then(
+            (body) => (spiderLinks(url, body, nesting)),
+            (err) => {
+                if(err.code !== 'ENOENT') {
+                    throw err;
+                }
+                return download(url, filename)
+                    .then(body => spiderLinks(url, body, nesting))
+                ;
+            }
+    );
+}
